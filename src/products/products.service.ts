29d078c8effaaa31930f43245d9ec4b2014817c9
Jhoan-pro <jdmunozm@unimayor.edu.co>
2026-09-04 15:40:58 -0500

    creacion de funcionalidad de los metodos THHP faltantes

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -7,25 +7,64 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
+  // Crear producto
   async create(createProductDto: CreateProductDto) {
     return await this.prisma.product.create({
       data: createProductDto,
     });
   }
 
-  findAll() {
-    return 'This action returns all products';
+  // Obtener todos los productos
+  async findAll() {
+    return await this.prisma.product.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  // Obtener un producto por ID
+  async findOne(id: number) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(
+        `Producto con ID ${id} no encontrado`,
+      );
+    }
+
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  // Actualizar producto
+  async update(
+    id: number,
+    updateProductDto: UpdateProductDto,
+  ) {
+    // Verificamos que exista
+    await this.findOne(id);
+
+    return await this.prisma.product.update({
+      where: {
+        id,
+      },
+      data: updateProductDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  // Eliminar producto
+  async remove(id: number) {
+    // Verificamos que exista
+    await this.findOne(id);
+
+    return await this.prisma.product.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
